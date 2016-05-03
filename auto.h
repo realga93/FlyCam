6861aceaabe327ce2adbf516db7a43c48110e28b
realga93 <realga93@tamu.edu>
2016-05-03 14:22:17 -0500

    MultiWii FlyCam

---
+++ auto.h
@@ -0,0 +1,30 @@
+/*
+ This file contains the values for the controls and the pins for the IR sensors.
+*/
+
+#ifndef AUTO_H_
+#define AUTO_H_
+
+#define THROTTLE_MAX 1977
+#define THROTTLE_MID 1494
+#define THROTTLE_MIN 982
+#define YAW_MAX 1995
+#define YAW_MID 1505
+#define YAW_MIN 1001
+#define PITCH_MAX 1912
+#define PITCH_MID 1405
+#define PITCH_MIN 982
+#define ROLL_MAX 1992
+#define ROLL_MID 1497
+#define ROLL_MIN 1011
+
+#define IR_FRONT_L 22
+#define IR_FRONT_R 23
+#define IR_LEFT_F 24
+#define IR_LEFT_B 25
+#define IR_RIGHT_F 26
+#define IR_RIGHT_B 27
+#define IR_BACK_L 28
+#define IR_BACK_R 29
+
+#endif
